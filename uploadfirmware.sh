dd56ceb83717f335b9b21dfc608328e297a668f5
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2019-05-10 10:04:39 +1000

    updated the uploadfirmware.sh script

--- uploadfirmware.sh
+++ uploadfirmware.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-md5sum  .pioenvs/nodemcuv2/firmware.bin |cut -d' ' -f 1 > .pioenvs/nodemcuv2/firmware.bin.md5
-cat .pioenvs/nodemcuv2/firmware.bin.md5
-rsync -aPh .pioenvs/nodemcuv2/firmware.bin.md5 root@xn--2xa.ink:/data/files/
-rsync -aPh .pioenvs/nodemcuv2/firmware.bin root@xn--2xa.ink:/data/files/firmware/$(md5sum  .pioenvs/nodemcuv2/firmware.bin | cut -d' ' -f1).bin
+MD5=$(md5sum  .pioenvs/nodemcuv2/firmware.bin |cut -d' ' -f 1)
+echo "$MD5" | ssh root@xn--2xa.ink -T "cat > /data/files/firmware.bin.md5"
+echo "$MD5 Updated firmware version file firmware.bin.md5"
+# rsync -aPh .pioenvs/nodemcuv2/firmware.bin root@xn--2xa.ink:/data/files/
+rsync -aPh .pioenvs/nodemcuv2/firmware.bin "root@xn--2xa.ink:/data/files/firmware/$MD5.bin"
+echo "$MD5 Uploaded firmware binary firmware/$MD5.bin"
+echo "$@" | \
+    ssh root@xn--2xa.ink \
+    -T "cat > /data/files/firmware/$MD5.precmd"
+echo "$MD5 Set precmd file firmware/$MD5.precmd"
